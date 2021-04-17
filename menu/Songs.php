405e979ddd92c54ca3b38ca41e098aab414dac59
Gomuro <75692732+Gomuro@users.noreply.github.com>
2021-04-17 10:54:57 +0300

    1.2

---
+++ menu/Songs.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="uk">
+  <head>
+
+      <link rel="stylesheet" href="Cords.css"/>
+    <title>GuitarProfi</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+<link rel="manifest" href="../favicons/site.webmanifest">
+<link rel="mask-icon" href="../favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+  </head>
+  <body>
+    <div class="header">
+        <p class="logo">GUITARPROFI   Платформа для навчання гри на гітарі №1</p>
+    </div>
+    <?php
+      include('menu.php')
+    ?>
+  
+  </body>
---
+++ menu/menu.php
@@ -0,0 +1,14 @@
+<div class="header">
+        <p class="logo">GUITARPROFI   Платформа для навчання гри на гітарі №1</p>
+    </div>
+      <div class="menu" id="sticky">
+        <ul>
+            <a href="../index.php" class="guitarProfi">GUITARPROFI</a>
+            <a href="Cords.php" class="link">Акорди</a>
+            <a href="Strumming.php" class="linkStrumming">Бої</a>
+            <a href="Lesons.php" class="link">Уроки гри</a>
+            <a href="Songs.php" class="link">Розбори пісень</a>
+            <a href="../articles/articles1.php" class="link">Про гітару</a>
+            <a href="../menu/BuyGuitar.php" class="link">Купити гітару</a>
+        </ul>
+      </div>
