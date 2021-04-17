b01233111a4cc020eac04f8a75de7291e1bd7df9
Gomuro <75692732+Gomuro@users.noreply.github.com>
2021-04-17 10:56:49 +0300

    1.3

---
+++ articles/articles1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="uk">
+  <head>
+
+      <link rel="stylesheet" href="articles1.css"/>
+    <title>GuitarProfi</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/favicon-16x16.png">
+<link rel="manifest" href="../favicons/site.webmanifest">
+<link rel="mask-icon" href="../favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+  </head>
+  <body>
+    <?php
+      include('menu.php')    
+    ?>
+  
+  <img src="../image/2list/1.jpg" class="topImage" alt="Не підтримується браузером">
+  <img src="../image/2list/2.jpg" class="rightImage" alt="Не підтримується браузером">
+  <div class="onImage">
+    <h2 class="textOnImage">Як вибрати першу гітару?</h2>
+  </div>
+  <div class="linkList">
+  <ul>
+    <li><a href="" class="linkRed">Акустична, класична чи електрогітара?</a>
+    <ul>
+      <li><a href="" class="linkRed">
+        Акустична
+      </a></li>
+      <li><a href="" class="linkRed">
+        Класична
+      </a></li>
+      <li><a href="" class="linkRed">
+        Електрогітара
+      </a></li>
+    </ul></li>
+    <li><a href="" class="linkRed">Яку гітару обрати?</a></li>
+    <li><a href="" class="linkRed">Лайфхаки, як вибрати хорошу гітару</a></li>
+    <li><a href="" class="linkRed"></a></li>
+  </ul> 
+</div>
+<div class="textOne">
+  <h2 class="topText">Акустична гітара</h2>
+  <p class="textContentOne">
+    На цьому виді гітари стоять металічні струни. 
+    Тому звук, який видає гітара, дзвінкіший (металічний) і багатший, ніж у класичній гітарі. 
+    Гриф на акустиці вужчий і довший. 
+    Корпус теж більший, що значно впливає на звук і на такій гітарі комфортніше грати, скажімо, на природі
+  </p>
+</div>
+</body>
---
+++ articles/menu.php
@@ -0,0 +1,14 @@
+<div class="header">
+        <p class="logo">GUITARPROFI   Платформа для навчання гри на гітарі №1</p>
+    </div>
+      <div class="menu" id="sticky">
+        <ul>
+            <a href="../index.php" class="guitarProfi">GUITARPROFI</a>
+            <a href="../menu/Cords.php" class="link">Акорди</a>
+            <a href="../menu/Strumming.php" class="linkStrumming">Бої</a>
+            <a href="../menu/Lesons.php" class="link">Уроки гри</a>
+            <a href="../menu/Songs.php" class="link">Розбори пісень</a>
+            <a href="../articles/articles1.php" class="link">Про гітару</a>
+            <a href="../menu/BuyGuitar.php" class="link">Купити гітару</a>
+        </ul>
+      </div>
