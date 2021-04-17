0a79f5e13da4fe0f4c9ce9977b4c6edc6a0c0247
Gomuro <75692732+Gomuro@users.noreply.github.com>
2021-04-17 10:52:04 +0300

    1.1

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="uk">
+  <head>
+
+      <link rel="stylesheet" href="GitarProfiMain.css"/>
+    <title>GuitarProfi</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+<link rel="manifest" href="favicons/site.webmanifest">
+<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+  </head>
+  <body>
+    <?php
+      include('menu.php')    
+    ?> 
+    <section class="showcase">
+       <video src="Guitare.mp4" muted loop autoplay></video>
+      <div class="overlay"></div>
+      <div class="introText">
+        <h2><strong>Як навчитися грати на гітарі?</strong></h2> 
+        <p>Легко! Напевно, це хотів би почути кожен. 
+          Та, як сказав один відомий гітарист, "потрібно бути дуже мужнім
+          , щоб почати вчитися чогось нового. 
+          Не буває зашвидко чи запізно, вчитися грати на гітарі. 
+          Акорди, бої, баре, перебори, розбори пісень - усього цього ви зможете навчитися з GuitarProfi</p>
+        <a href="../site/menu/Lesons.php"><strong>Почати навчання!</strong></a>
+        <div class="descripiton">
+        </div>
+      </div>
+      <ul class="social">
+        <li><a href="#"><img src="image/facebook.png"></a></li>
+        <li><a href="#"><img src="image/twitter.png"></a></li>
+        <li><a href="#"><img src="image/instagram.png"></a></li>
+      </ul>
+    </section>
+    
+    
+  <div class="linkList">
+      <p class="topList">У цій статті:</p>
+      <ul>
+        <li><a href="#CanIplay"class="linkRed">Чи можу я навчитися грати на гітарі самостійно?</a></li>
+        <li><a href="#" class="linkRed">Постановка рук на гітарі</a></li>
+        <li><a href="#" class="linkRed">Настройка гітари</a></li>
+        <li><a href="#HowPlay" class="linkRed">Які є основні акорди, бої та перебори на гітарі?</a></li>
+        <li><a href="#" class="linkRed">Вправи на техніку</a></li>
+        <li><a href="#" class="linkRed">Як розучувати пісні</a></li>
+      </ul> 
+      <p class="bottomList">Та інше</p>   
+  </div>
+   <img class="imgOne" src="image/1.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgTwo" src="image/2.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgThree" src="image/3.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgFour" src="image/4.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgFive" src="image/5.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgSix" src="image/6.jpg" alt="Зображення не підтримується Браузером"> 
+   <img class="imgSeven" src="image/7.jpg" alt="Зображення не підтримується Браузером"> 
+   <img class="imgEight" src="image/8.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgNine" src="image/9.jpg" alt="Зображення не підтримується Браузером">
+   <img class="imgTen" src="image/10.jpg" alt="Зображення не підтримується Браузером">
+  <h3 class="textUnderPicture" ><a class="hid1" id="CanIplay">Чи можу я навчитися<br> грати на гітарі самостійно?</a></h3>
+  <div class="textOne">
+    <p class="textContentOne">Як навчитися грати на гітарі? Над цим питанням задумувався практично кожен. І це
+      зрозуміло — бо одні ходять роками в музичну школу, інші наймають викладача, а ще
+      інші вчаться самі (і в них це непогано виходить). Підтримуючи запал до музики і трохи
+      терпіння — можна досягти цієї мети усіма переліченими способами. Самодисципліна
+      — це лишень у ваших руках, але щоб робити швидший прогрес, вам необхідна
+      допомога експертів. Вони направлять у правильне русло. На цьому сайті ви знайдете
+      вдосталь матеріалу, який допоможе вам почати самостійно грати на гітарі. Дізнаєтесь
+      більше про основи акустичної та електричної та інші хитрощі. Ми намагалися охопити
+      все потрібне. Ці уроки будуть корисні не тільки початківцям, а й тим, хто вже довший
+      час грає на гітарі.</p>
+  </div>
+  <div class="textTwo">
+      <h2 class="topText">Постановка рук на гітарі</h2>
+      <p class="textContentTwo">Це є найважливіше для кожного музиканта. Всі уроки, які наведені нижче,
+        присвячені цьому — правильній постановці рук. Вони пояснюють, як
+        правильно тримати гітару, в якому положенні мають бути пальці та інше.</p>
+  </div>
+  <div class="listHendsOnGuitar">
+    <ul>
+      <li><a href="#" class="linkRed">Як правильно тримати гітару.</a></li>
+      <li><a href="#" class="linkRed">Постановка правої руки.</a></li>
+      <li><a href="#" class="linkRed">Постановка лівої руки.</a></li>
+      <li><a href="#" class="linkRed">Навіщо гітаристу нігті і що робити з мозолями.</a></li>
+    </ul>
+  </div>
+  <div class="textThree">
+    <h2 class="topText">Настройка гітари</h2>
+    <p class="textContentThree">Що робити, коли гітара розстроєна і не звучить? Тут початківці, хто і поняття не
+      має як настроїти свій інструмент, зможуть дізнатися, як це правильно робити.</p>
+  </div>
+  <div class="listGuitarTuning">
+    <ul>
+      <li><a href="#" class="linkRed">Як настроїти гітару: 6 способів</a></li>
+      <li><a href="#" class="linkRed">Програми для настройки гітари</a></li>
+    </ul>
+  </div>
+  <h3 class="textUnderPicture" ><a class="hid1" id="HowPlay">Які є основні акорди,<br> бої та перебори на гітарі?</a></h3>
+  <div class="textFour">
+    <h2 class="topText">Акорди</h2>
+    <p class="textContentFour">Почнемо з акордів. Акорди — це група нот, які звучать одночасно, щоб
+      отримати гармонійний звук. Навіть коли ти не можеш грати так майстерно і
+      блискавично соло, як твій улюблений гітарист (можливо Ed Sheeran чи
+      Passenger), ти все одно зможеш грати партії їхніх пісень, якщо знаєш
+      акорди. Які є основні акорди? Як їх зажимати? Що таке баре? Дивися нижче)</p>
+  </div>
+  <div class="Chords">
+    <ul>
+      <li><a href="#" class="linkRed">Як читати аплікатури акордів?</a></li>
+      <li><a href="#" class="linkRed">Як зажимати акорди?</a></li>
+      <li><a href="#" class="linkRed">Як правильно брати баре?</a></li>
+    </ul>
+  </div>
+  <div class="textFive">
+    <h2 class="topText">Основні бої</h2>
+    <p class="textContentFive">Щоб наші акорди зазвучали, найпростіше нам допоможе бій на гітарі.
+       Думаю, що і початківцям відомо, що це таке. 
+       Це цикл рухів, коли б’єш одночасно по декількох струнах і які постійно повторюються. 
+       Подивіться статті нижче і ознайомтеся з різними боями на гітарі.
+    </p>
+  </div>
+  <div class="Strumming">
+    <ul>
+      <li><a href="#" class="linkRed">Бої на гітарі. 12 видів</a></li>
+      <li><a href="#" class="linkRed">Бій «Четвірка»</a></li>
+      <li><a href="#" class="linkRed">Бій «Шестірка»</a></li>
+      <li><a href="#" class="linkRed">Бій «Вісімка»</a></li>
+      <li><a href="#" class="linkRed">Ритмічні малюнки</a></li>
+      <li><a href="#" class="linkRed">Як підібрати бій до пісні</a></li>
+    </ul>
+  </div>
+  <div class="textSix">
+    <h2 class="topText">Види переборів</h2>
+    <p class="textContentSix">
+      Наступні статті присвячені переборам. Таке звукоутворення сильно відрізняється
+      від гри боєм. Його ще грамотно називають арпеджіо (італ. arpeggio - грати як на
+      арфі). Нижче подаються як класичні, так і інші красиві патерни, або малюнки.
+    </p>
+  </div>
+  <div class="Scales">
+    <ul>
+      <li><a href="#" class="linkRed">Перебори для початківців.</a></li>
+      <li><a href="#" class="linkRed">Види переборів.</a></li>
+      <li><a href="#" class="linkRed">Одні з найкрасивіших переборів.</a></li>
+    </ul>
+  </div>
+  <div class="textSeven">
+    <h2 class="topText">Вправи на техніку</h2>
+    <p class="textContentSeven">
+      Якщо ви вирішили серйозно займатися
+      музикою і плануєте в майбутньому
+      грати серйозні композиції, то вам
+      потрібно виділяти час на тренування.
+      Наберіться терпіння) Майстерність
+      завжди вимагає великих затрат часу і
+      сил. Нижче наведені вправи для
+      розвитку швидкості і чистоти гри на
+      гітарі. Також зможете ознайомитися з
+      теорією музики, з стандартами для різних жанрів, ну і звичайно ж, як самому
+      імпровізувати.
+    </p>
+  </div>
+  
+  <div class="Ex">
+    <ul>
+      <li><a href="" class="linkBlue">Вправи на техніку для початківців</a></li>
+      <li><a href="" class="linkBlue">Вправи на розвивання пальців рук</a></li>
+      <li><a href="" class="linkBlue">Вправи на розтяжку пальців</a></li>
+      <li><a href="" class="linkBlue">Як грати рок?</a></li>
+      <li><a href="" class="linkBlue">Як грати блюз?</a></li>
+      <li><a href="" class="linkBlue">Як грати рок-н-ролл?</a></li>
+      <li><a href="" class="linkBlue">Як імпровізувати?</a></li>
+      <li><a href="" class="linkBlue">Ноти на грифі?</a></li>
+      <li><a href="" class="linkBlue">Як читати табулятури або таби?</a></li>
+      <li><a href="" class="linkBlue">Лайфхаки для гітаристів</a></li>
+    </ul>
+  </div>
+  <div class="lernSongs">
+    <h2 class="topText">Як розучувати пісні?</h2>
+    <p class="textContentEight"><strong>Для початківця це може стати складним завданням. Але всі переходили
+      через це. Тому, переходимо до практики — розбору пісень на гітарі.
+    </strong>
+    </p>  
+  </div>
+  <div class="whatWeLern">
+    <h2 class="topText">Підсумуємо, що ми дізналися.</h2>
+    <p class="result">
+      Ми ознайомилися з основами гри на гітарі. Принципи, які тут містяться,
+    підходять як для акустичної, так і електичної гітари. Вивчайте акорд за
+    акордом, бій за боєм, і ви побачите, що грати на гітарі простіше, ніж ви собі
+    думали.<br><br>
+    Що робити, коли ви вже освоїли ці основи? Відповідь проста — продовжуте
+вправлятися. Постійно вчіться чомусь новому — бо в грі на гітарі не можна досягти вершини, коли
+вже немає куди рухатися. В Андрія Сеговія, відомого класичного гітариста минулого століття, на
+старості літ запитали: «На скільки добре ви вивчили цей інструмент?». Його відповідь: «Десь на
+13%.». А він, хочеться згадати, грав на гітарі з 4 років. Пропонуємо вам серію уроків гри на гітарі від
+GuitarProfi. Можливо, саме ви станете другим Сеговією.
+    </p>
+    <p class="Guide"> 
+      Більше статей В цьому курсі   
+  </p>
+  </div>
+  <div>
+    <a href="menu/Lesons.html">Як вибрати гітару</a>
+  </div>
+  <div class="futter">
+    <span class="futterText">
+      "Copyright ©"
+      <time id="copyright-year">2021</time>
+      ". GuitarProfi. All Rights Reserved."
+    </span>
+  </div>
+  
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<div class="header">
+        <p class="logo">GUITARPROFI   Платформа для навчання гри на гітарі №1</p>
+    </div>
+      <div class="menu" id="sticky">
+        <ul>
+            <a href="../site/index.php" class="guitarProfi">GUITARPROFI</a>
+            <a href="../site/menu/Cords.php" class="link">Акорди</a>
+            <a href="../site/menu/Strumming.php" class="linkStrumming">Бої</a>
+            <a href="../site/menu/Lesons.php" class="link">Уроки гри</a>
+            <a href="../site/menu/Songs.php" class="link">Розбори пісень</a>
+            <a href="../site/articles/articles1.php" class="link">Про гітару</a>
+            <a href="../site/menu/BuyGuitar.php" class="link">Купити гітару</a>
+        </ul>
+      </div>
